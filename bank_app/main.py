b1e3f537afa88a3804345956b506022d94ce640f
sampauly <samuelpauly02@gmail.com>
2026-08-13 13:54:18 -0400

    basic react frontend working with customer endpoints

--- bank_app/main.py
+++ bank_app/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from controllers.customer_controller import router as customer_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(customer_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
